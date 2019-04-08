0a6f9cb9b7c6fbb8533c9106e911be64f93d4bbd
SayBGM <bgm537@saybgm.me>
2019-04-08 15:28:08 +0900

    typescript config

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export interface Props {
+  
+}
+ 
+export interface State {
+  
+}
+ 
+class App extends React.Component<Props, State> {
+  render() { 
+    return (
+      <div>
+        가나다
+      </div>
+    );
+  }
+}
+ 
+export default App;
