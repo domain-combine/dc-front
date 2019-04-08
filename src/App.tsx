3438da99a6e31f1c2d2e71ed6eb0b69ba3ff9e2e
SayBGM <bgm537@saybgm.me>
2019-04-08 14:16:52 +0900

    TS react setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export interface Props {
+  
+}
+ 
+export interface State {
+  
+}
+ 
+class App extends React.Component<Props, State> {
+  render() { 
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+ 
+export default App;
