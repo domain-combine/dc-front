d6f622fc034c91d2aa9c957d643599e7d006009f
SayBGM <bgm537@saybgm.me>
2019-04-08 14:56:40 +0900

    resetup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import * as React from 'react';
+import './App.css';
 
-export interface Props {
-  
-}
- 
-export interface State {
-  
-}
- 
-class App extends React.Component<Props, State> {
-  render() { 
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
     return (
-      <div>
-
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
       </div>
     );
   }
 }
- 
+
 export default App;
