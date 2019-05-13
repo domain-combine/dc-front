7478f29c4a39c9498ce68a725eff1d70b5e5a099
SayBGM <bgm537@saybgm.me>
2019-05-13 15:54:48 +0900

    MainSearchBar 개선

--- src/components/Main/MainSearchBar.tsx
+++ src/components/Main/MainSearchBar.tsx
@@ -1,39 +1,36 @@
-import React, { useState } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import classNames from 'classnames';
-import SearchBarTLD from './SearchBarTLD';
+import React, { useState } from "react";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import classNames from "classnames";
+import SearchBarTLD from "./SearchBarTLD";
 
-import './MainSearchBar.scss';
+import "./MainSearchBar.scss";
 
-interface MainSearchBarProps extends RouteComponentProps<any>{
-}
+interface MainSearchBarProps extends RouteComponentProps<any> {}
 
-type InputEvent = React.ChangeEvent<HTMLInputElement>;
-
-const MainSearchBar = ({history, location, match}:MainSearchBarProps) => {
-  const [ searchDomain, setDomain ] = useState<string>('');
+const MainSearchBar = ({ history, location, match }: MainSearchBarProps) => {
+  const [searchDomain, setDomain] = useState<string>("");
 
   return (
     <div className="MainSearchBar">
       <form
         className="MainSearchBar__InputBox"
-        onSubmit={(e:any) => {
+        onSubmit={(e: any) => {
           e.preventDefault();
           history.push(`/search/${searchDomain}`);
-        }
-      }>
+        }}
+      >
         <span className="MainSearchBar__InputBox__title">www.</span>
-        <input 
+        <input
           type="text"
           className="MainSearchBar__InputBox__input"
           value={searchDomain}
           placeholder="구매하고 싶은 도메인을 입력하세요"
-          onChange={(e:InputEvent) => setDomain(e.target.value)}
+          onChange={(e: InputEvent) => setDomain(e.target.value)}
         />
         <SearchBarTLD />
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default withRouter(MainSearchBar);
