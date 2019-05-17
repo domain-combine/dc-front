ef6df2e931ffde0eaa2c19db0a83067369240761
SayBGM <bgm537@saybgm.me>
2019-05-17 11:18:32 +0900

    sellerLogo 추가

---
+++ src/assets/sellerLogo/sellerLogo.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { ReactComponent as BlueHost } from "./BlueHost.svg";
+import { ReactComponent as DirectHosting } from "./DirectHosting.svg";
+import { ReactComponent as NameDotCom } from "./NameDotCom.svg";
+
+interface SellerLogoProps {
+  className: string;
+  id: string | undefined;
+}
+
+const Gabia = ({ className, id }: SellerLogoProps) => (
+  <img
+    className={className}
+    id={id}
+    src={require("./Gabia.png")}
+    alt="가비아 로고"
+  />
+);
+
+const GoDaddy = ({ className, id }: SellerLogoProps) => (
+  <img
+    className={className}
+    id={id}
+    src={require("./GoDaddy.png")}
+    alt="고대디 로고"
+  />
+);
+
+const HostingKr = ({ className, id }: SellerLogoProps) => (
+  <img
+    className={className}
+    id={id}
+    src={require("./HostingKr.png")}
+    alt="HostingKr 로고"
+  />
+);
+
+const MailPlug = ({ className, id }: SellerLogoProps) => (
+  <img
+    className={className}
+    id={id}
+    src={require("./MailPlug.png")}
+    alt="MailPlug 로고"
+  />
+);
+
+const OnlyDomains = ({ className, id }: SellerLogoProps) => (
+  <img
+    className={className}
+    id={id}
+    src={require("./OnlyDomains.png")}
+    alt="OnlyDomains 로고"
+  />
+);
+
+export {
+  BlueHost,
+  DirectHosting,
+  NameDotCom,
+  Gabia,
+  GoDaddy,
+  HostingKr,
+  MailPlug,
+  OnlyDomains
+};
