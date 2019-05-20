38b4c085396e34edf7e26edb9dead1a2fa1c6c25
SayBGM <bgm537@saybgm.me>
2019-05-20 22:28:41 +0900

    SearchPageSearchInput 추가

--- src/container/SearchPage.tsx
+++ src/container/SearchPage.tsx
@@ -1,16 +1,18 @@
-import * as React from 'react';
-import SearchPageBar from '../components/SearchPage/SearchPageBar';
+import * as React from "react";
+import SearchPageBar from "../components/SearchPage/SearchPageBar";
+import SearchPageSearchInput from "../components/SearchPage/SearchPageSearchInput";
 
-interface SearchPageProps {
+import "./SearchPage.scss";
 
-}
+interface SearchPageProps {}
 
-const SearchPage = ({}:SearchPageProps) => {
+const SearchPage = ({  }: SearchPageProps) => {
   return (
-    <div className="SearchPage">
-      <SearchPageBar tldList={['']}/>
+    <div className="SearchPage" style={{ width: window.screen.width }}>
+      <SearchPageBar tldList={[""]} />
+      <SearchPageSearchInput />
     </div>
-  )
-}
+  );
+};
 
 export default SearchPage;
