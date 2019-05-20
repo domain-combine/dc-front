5072c6a5f0ef2485a8e00426402c2e5bb4379abf
SayBGM <bgm537@saybgm.me>
2019-05-20 23:41:36 +0900

    SearchPage에 SearchPageRender 추가

--- src/container/SearchPage.tsx
+++ src/container/SearchPage.tsx
@@ -1,18 +1,32 @@
-import * as React from "react";
-import SearchPageBar from "../components/SearchPage/SearchPageBar";
-import SearchPageSearchInput from "../components/SearchPage/SearchPageSearchInput";
+import * as React from 'react';
+import { withRouter, RouteComponentProps, match } from 'react-router-dom';
+import SearchPageBar from '../components/SearchPage/SearchPageBar';
+import SearchPageSearchInput from '../components/SearchPage/SearchPageSearchInput';
+import SearchPageResultRender from '../components/SearchPage/SearchPageResultRender';
 
-import "./SearchPage.scss";
+import './SearchPage.scss';
 
-interface SearchPageProps {}
+interface SearchPageProps extends RouteComponentProps {
+	match: match<RouterMatch>;
+}
 
-const SearchPage = ({  }: SearchPageProps) => {
-  return (
-    <div className="SearchPage" style={{ width: window.screen.width }}>
-      <SearchPageBar tldList={[""]} />
-      <SearchPageSearchInput />
-    </div>
-  );
+interface RouterMatch {
+	domain: string;
+}
+
+const SearchPage = ({ match }: SearchPageProps) => {
+	const [SearchResult, setSearchResult] = React.useState([]);
+	React.useEffect(() => {});
+
+	return (
+		<div className="SearchPage" style={{ width: window.screen.width }}>
+			<SearchPageBar tldList={['']} />
+			<div className="SearchPage__wrapper">
+				<SearchPageSearchInput />
+				<SearchPageResultRender domainTLDList={SearchResult} />
+			</div>
+		</div>
+	);
 };
 
-export default SearchPage;
+export default withRouter(SearchPage);
