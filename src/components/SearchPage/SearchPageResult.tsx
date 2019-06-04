726c2c559d048d929595035595bcca6e7052b95d
GwangMin <bgm537@saybgm.me>
2019-06-04 11:17:23 +0900

    구매하기를 누르면 도메인 이름이 클립보드로 추가됨

--- src/components/SearchPage/SearchPageResult.tsx
+++ src/components/SearchPage/SearchPageResult.tsx
@@ -13,12 +13,29 @@ const SearchPageResult = ({ domainName, domainInfo }: SearchPageResultProps) => 
 	if (!domainInfo.view) {
 		return null;
 	}
+
+	function copyToClipboard(val: string) {
+		var t = document.createElement('textarea');
+		document.body.appendChild(t);
+		t.value = val;
+		t.select();
+		document.execCommand('copy');
+		document.body.removeChild(t);
+	}
+
 	return (
 		<div className="SearchPageResult">
 			<DomainSellerLogo origin={origin} />
 			<div className="SearchPageResult__domainName">{`${domainName}.${tld}`}</div>
 			<div className="flex" />
-			<div className="SearchPageResult__PriceInfo">
+			<div
+				className="SearchPageResult__PriceInfo"
+				onClick={() => {
+					copyToClipboard(`${domainName}.${tld}`);
+					alert('도메인 이름을 클립보드에 복사하였습니다. 페이지 이동 후 붙여넣어주세요.');
+					window.open(domainInfo.origin);
+				}}
+			>
 				<div className="SearchPageResult__PriceInfo__wrapper">
 					총 {length}개 사이트 중 최저가
 					<div className="SearchPageResult__PriceInfo__wrapper__price">
